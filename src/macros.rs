66ab66ef9716d0143ba3244237bd4ad435fba7f3
Mateusz Biel <matbiel@gmail.com>
2024-09-08 23:25:27 +0200

    Add simple macro matching type

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,34 @@
 pub mod macros {
+    use std::fmt::Display;
+
     #[macro_export]
     macro_rules! count_tts {
         () => { 0 };
         ($odd:tt $($a:tt $b:tt)*) => { (count_tts!($($a)*) << 1) | 1 };
         ($($a:tt $even:tt)*) => { count_tts!($($a)*) << 1 };
+    }
+
+    #[macro_export]
+    macro_rules! vector {
+        ($t:ty) => {
+            {
+                let vec: Vec<$t> = Vec::with_capacity(8);
+                vec
+            }
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vector;
+
+    #[test]
+    fn should_create_vector_of_specified_type() {
+        let mut result: Vec<Box<i32>> = vector!(Box<i32>);
+
+        result.push(Box::new(7));
+
+        assert_eq!(*result[0], 7);
     }
 }
